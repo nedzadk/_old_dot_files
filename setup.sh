4831bf3bcd0a41c6ab3f2df5ba601dcf9c511243
Nedzad Kulelija <nedzadk@gmail.com>
2019-02-16 00:25:01 +0100

    Added short symlink setups script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+ln -s ${PWD}/i3/config ~/.config/i3/config
+ln -s ${PWD}/i3/i3status.conf ~/.config/i3/i3status.conf
+ln -s ${PWD}/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ${PWD}/xresources/dot-Xresources ~/.Xresources
+ln -s ${PWD}/i3scripts ~/i3scripts
