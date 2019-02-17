93f335de6a1f2fd94581a2fdb608b9a25e10e405
Nedzad Kulelija <nedzadk@gmail.com>
2019-02-17 01:03:03 +0100

    Added javascript snippets for vim UltiSnips

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 ln -s ${PWD}/i3/config ~/.config/i3/config
 ln -s ${PWD}/i3/i3status.conf ~/.config/i3/i3status.conf
 ln -s ${PWD}/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ${PWD}/nvim/UltiSnips ~/.config/nvim/UltiSnips
 ln -s ${PWD}/xresources/dot-Xresources ~/.Xresources
 ln -s ${PWD}/i3scripts ~/i3scripts
