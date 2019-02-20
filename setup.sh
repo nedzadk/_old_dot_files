bb237f910b700104aee7322bc589b813d03544b7
Nedzad Kulelija <nedzadk@gmail.com>
2019-02-20 05:32:33 +0100

    tools for controling spotify from command line

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ ln -s ${PWD}/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ${PWD}/nvim/UltiSnips ~/.config/nvim/UltiSnips
 ln -s ${PWD}/xresources/dot-Xresources ~/.Xresources
 ln -s ${PWD}/i3scripts ~/i3scripts
+
+# Control spotify 
+ln -s ${PWD}/tools/sp /usr/bin/sp
