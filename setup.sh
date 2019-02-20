a652ba1207c64d7a8cf639b18a3e5d2cfd356200
Nedzad Kulelija <nedzadk@gmail.com>
2019-02-20 05:41:23 +0100

    Dunst[notifications] config

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ ln -s ${PWD}/nvim/UltiSnips ~/.config/nvim/UltiSnips
 ln -s ${PWD}/xresources/dot-Xresources ~/.Xresources
 ln -s ${PWD}/i3scripts ~/i3scripts
 ln -s ${PWD}/compton/compton.conf ~/.config/compton.conf
+ln -s ${PWD}/dunst/dunstrc ~/.config/dunst/dunstrc
 
 # Control spotify 
 ln -s ${PWD}/tools/sp /usr/bin/sp
