9d43b6a5194c8cccfff3d34eb95cda6f4953a3c8
Nedzad Kulelija <nedzadk@gmail.com>
2019-03-22 13:46:59 +0100

    Xresources commented out, and touchpad configuration added

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ ln -s ${PWD}/i3/config ~/.config/i3/config
 ln -s ${PWD}/i3/i3status.conf ~/.config/i3/i3status.conf
 ln -s ${PWD}/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ${PWD}/nvim/UltiSnips ~/.config/nvim/UltiSnips
-ln -s ${PWD}/xresources/dot-Xresources ~/.Xresources
+# ln -s ${PWD}/xresources/dot-Xresources ~/.Xresources
 ln -s ${PWD}/i3scripts ~/i3scripts
 ln -s ${PWD}/compton/compton.conf ~/.config/compton.conf
 ln -s ${PWD}/dunst/dunstrc ~/.config/dunst/dunstrc
